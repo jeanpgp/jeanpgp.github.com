51d9d3c97344b10c6436981c8b4f15735b384d40
Jean <jgpagaduan@ucdavis.edu>
2021-05-25 01:54:45 -0700

    astudio3

--- studio3/script.js
+++ studio3/script.js
@@ -3,10 +3,14 @@ const startGame = document.getElementById('startgame');
 const gameControl = document.getElementById('gamecontrol');
 const game = document.getElementById('game');
 const score = document.getElementById('score');
-const actionArea = document.getElementById('actions');   
+const actionArea = document.getElementById('actions');  
+
+const diceRoll = new Audio('media/diceroll.mp3');
+const pass = new Audio('media/pass.mp3');
+
 const gameData = {
-    dice: ['1die.png', '2die.png', '3die.png', 
-        '4die.png', '5die.png', '6die.png'],
+    dice: ['images/1die.png', 'images/2die.png', 'images/3die.png', 
+        'images/4die.png', 'images/5die.png', 'images/6die.png'],
     players: ['player 1', 'player 2'],
     score: [0, 0],
     roll1: 0,
@@ -42,9 +46,8 @@ function throwDice(){
     game.innerHTML = `<p>Order breakfast for the ${gameData.players[gameData.index]}</p>`;
     game.innerHTML+= `<img src="${gameData.dice[gameData.roll1-1]}">
                         <img src="${gameData.dice[gameData.roll2-1]}">`;
-    
+                        diceRoll.play();
     gameData.rollSum=gameData.roll1 + gameData.roll2;
-    console.log(gameData);
     if(gameData.rollSum === 2){
         console.log("2 fried eggs were ordered");
         game.innerHTML+= '<p>Oh no! 2 fried eggs!</p>';
@@ -63,16 +66,14 @@ function throwDice(){
     else{
         console.log("the game proceeds");
         gameData.score[gameData.index] = gameData.score[gameData.index] + gameData.rollSum;
-        actionArea.innerHTML = '<button id= "rollagain">Order again</button> or <button id="pass">Pass</button>';
+        actionArea.innerHTML = '<button id= "rollagain">Order again</button> <button id="pass">Pass</button>';
         document.getElementById('rollagain').addEventListener('click', function(){
-            setUpTurn();
-        });
-        document.getElementById('rollagain').addEventListener('click',function(){
             setUpTurn();
         });
         document.getElementById('pass').addEventListener('click', function(){
             gameData.index ? (gameData.index = 0) : (gameData.index=1);
             setUpTurn();
+            pass.play();
         });
         checkWinningCondition();
     }
@@ -85,14 +86,14 @@ function checkWinningCondition(){
         document.getElementById('quit').innerHTML= "Start a new game?";
     }
     else{
-        score.innerHTML=`<p> the score is currently <strong>${gameData.players[0]}
-            ${gameData.score[0]}</strong> and  <strong>${gameData.players[1]}
+        score.innerHTML=`<p> the score is currently <strong>${gameData.players[0]} :
+            ${gameData.score[0]}</strong> and  <strong>${gameData.players[1]} : 
             ${gameData.score[1]}</strong> </p>`;
     }
 }
 function showCurrentScore(){
-    score.innerHTML=`<p> the score is currently <strong>${gameData.players[0]}
-            ${gameData.score[0]}</strong> and  <strong>${gameData.players[1]}
+    score.innerHTML=`<p> the score is currently <strong>${gameData.players[0]} :
+            ${gameData.score[0]}</strong> and  <strong>${gameData.players[1]} :
             ${gameData.score[1]}</strong> </p>`;
 
 }
